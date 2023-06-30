5f2fa83e3d65a2ad51be55f4f3a5582895e5634a
surade2001 <swapnilurade2001@gmail.com>
2023-06-30 09:10:48 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router } from "@reach/router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Login from "./components/login";
+import Dashboard from "./components/dashboard";
 
-export default App;
+const Routes = () => (
+    <Router>
+        <Login path="login" />
+        <Dashboard path="dashboard" />
+    </Router>
+);
+
+export default Routes;
